9873ac5fc84a104e66c34d9683dc46cf0ffd9c40
Luke Barbuto <luke.barbuto@gmail.com>
2016-11-30 10:59:21 -0800

    Install react-router 4

--- app/templates/client/modules/app/components/App/index.js
+++ app/templates/client/modules/app/components/App/index.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react'
-import logo from './logo.png'
-import styles from './styles.scss'
+import React from 'react'
+import { BrowserRouter, Match, Miss } from 'react-router'
 
-class App extends Component {
-  render() {
-    return (
+import styles from './styles.scss'
+import logo from './logo.png'
+
+export default function App() {
+  return (
+    <BrowserRouter>
       <div className={styles.root}>
         <div className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
+          <img src={logo} className={styles.logo} alt='logo' />
           <h2>Welcome!</h2>
         </div>
+
+        <Match exactly pattern='/' component={Home} />
+        <Miss component={NotFound} />
       </div>
-    )
-  }
+    </BrowserRouter>
+  )
 }
 
-export default App
+function Home() {
+  return (
+    <h3>Home</h3>
+  )
+}
+
+function NotFound() {
+  return (
+    <h3>Route not found</h3>
+  )
+}
