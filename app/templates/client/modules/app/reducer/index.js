864d2ea9c7440e862155273ea4c5f5d2ba03ce94
Byron Matto <bmatto@gmail.com>
2016-12-01 13:31:49 -0500

    Add Zeal Redux Utils

--- app/templates/client/modules/app/reducer/index.js
+++ app/templates/client/modules/app/reducer/index.js
@@ -1,3 +1,10 @@
-export default function reducer(state = {}, _action) {
-  return state
+import { createReducer } from 'zeal-redux-utils'
+
+const initialState = {}
+
+const handlers = {
+  // Pattern:
+  // [ActionTypes.ACTION_NAME]: actionFunction
 }
+
+export default createReducer(initialState, handlers)
