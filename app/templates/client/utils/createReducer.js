539300ec93dc3dcbf08b40d4e04948c8866f959e
Randy Coulman <rcoulman@gmail.com>
2017-05-12 09:27:16 -0700

    Don't check apollo actions for FSA compliance
    Apollo’s redux actions don’t conform to the flux-standard-action spec,
    but zeal-redux-utils’ `createReducer` throws an error if any
    non-FSA-compliant actions are dispatched.
    
    zeal-redux-utils 0.3.1 now allows a whitelist function to be passed
    which will bypass the FSA check on whitelisted actions.
    
    We add a local `createReducer` function that supplies an appropriate
    whitelist function for Apollo actions and the use that everywhere.
    
    NOTE: This new function makes use of Ramda, so we now include that in
    this project.

---
+++ app/templates/client/utils/createReducer.js
@@ -0,0 +1,9 @@
+import { partialRight, test } from 'ramda'
+import { createReducer } from 'zeal-redux-utils'
+
+const isApolloAction = action => test(/^APOLLO_/, action.type)
+
+export default partialRight(
+  createReducer,
+  [{ allowNonStandardActionIf: isApolloAction }]
+)
